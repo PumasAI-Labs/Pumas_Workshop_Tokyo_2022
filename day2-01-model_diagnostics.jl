6e5c2f1821f44d1d5d657c965a093c14b64636fe
Andreas Noack <andreas@noack.dk>
2022-10-30 08:33:12 -0600

    Add the rest of Patrick's script

--- day2-01-model_diagnostics.jl
+++ day2-01-model_diagnostics.jl
@@ -74,16 +74,137 @@ covariates_check(model, pop) # can also pass fit or inspect
 covariates_dist(model, pop) # can also pass fit or inspect
 
 # Parameter values
-params = (
-    tvvc = 5,
-    tvcl = 0.02,
-    tvq = 0.01,
-    tvvp = 10,
-    allometric = 1.0,
-    Ω = Diagonal([0.01, 0.01]),
-    σ = 0.01,
-)
+params =
+    (tvvc = 5, tvcl = 0.02, tvq = 0.01, tvvp = 10, allometric = 1.0, Ω = Diagonal([0.01, 0.01]), σ = 0.01)
 
 # Check for finite loglikelihood
 loglikelihood(model, pop, params, Pumas.FOCE()) # using model + population + params + estimation method
 findinfluential(model, pop, params, Pumas.FOCE(); k = 30)
+
+# Fit model
+fit_results_base = fit(model, pop, params, Pumas.FOCE(); constantcoef=(allometric=0.0,))
+
+# Model metrics in a DataFrame
+fit_results_base_metrics = metrics_table(fit_results_base)
+
+# Loglikelihood and NONMEM's OFV with constant
+loglikelihood(fit_results_base) # using a result from fit
+
+# to reproduce NONMEM's "OFV with constant" you need to divide by -2
+loglikelihood(fit_results_base) / -2
+
+# AIC
+aic(fit_results_base)
+
+# BIC
+bic(fit_results_base)
+
+# ϵ-Shinkrage
+ϵshrinkage(fit_results_base)
+
+#η-Shinkrage
+ηshrinkage(fit_results_base)
+
+# Inspect the models
+# this runs predictions, wres, ebes, icoef and dosecontrol
+fit_inspect_base = inspect(fit_results_base)
+
+# Open the app
+inspect_app = evaluate_diagnostics(fit_inspect_base)
+
+# output in a handy DataFrame format
+fit_inspect_df = DataFrame(fit_inspect_base)
+# too many columns... workspace browser...
+
+# you can get any of the single results using the functions
+DataFrame(predict(fit_results_base))
+DataFrame(wresiduals(fit_results_base))
+DataFrame(icoef(fit_results_base))
+
+# Plot individual parts
+
+## Goodness of Fit plots
+goodness_of_fit(fit_inspect_base)
+observations_vs_predictions(fit_inspect_base)
+observations_vs_ipredictions(fit_inspect_base)
+wresiduals_vs_time(fit_inspect_base)
+iwresiduals_vs_ipredictions(fit_inspect_base)
+
+empirical_bayes_vs_covariates(fit_inspect_base)
+interactive(empirical_bayes_vs_covariates(fit_inspect_base))
+
+# Setting options for plots
+empirical_bayes_vs_covariates(fit_inspect_base;
+    figure = (fontsize=11,),
+    color=:red)
+
+empirical_bayes_vs_covariates(fit_inspect_base;
+    figure = (fontsize=11,),
+    color=:red,
+    markersize=5,)
+
+# Fit allometric scaling model
+fit_results_allometric = fit(model, pop, params, Pumas.FOCE(); constantcoef=(allometric=1.0,))
+compare_estimates(;fit_results_base, fit_results_allometric)
+
+fit_inspect_allometric = inspect(fit_results_allometric)
+
+goodness_of_fit(fit_inspect_allometric)
+observations_vs_predictions(fit_inspect_allometric)
+empirical_bayes_vs_covariates(fit_inspect_allometric)
+
+# Inference
+## Confidence Intervals using asymptotic variance-covariance
+fit_infer = infer(fit_results_allometric)
+coeftable(fit_results_allometric)  # DataFrame
+stderror(fit_infer)
+
+## Confidence Intervals using bootstrap
+fit_infer_bs = infer(fit_results_allometric, Pumas.Bootstrap(samples = 100))
+coeftable(fit_infer_bs)
+stderror(fit_infer)
+DataFrame(fit_infer_bs.vcov)
+
+## Stratified sampling of subjects
+fit_infer_bs_stratify = infer(fit_results_allometric, Pumas.Bootstrap(samples = 100, stratify_by=:WT))
+coeftable(fit_infer_bs_stratify)
+stderror(fit_infer)
+
+# Seed
+rng = Random.seed!(2131)
+fit_infer_bs_rng_1 = infer(fit_results_allometric, Pumas.Bootstrap(;samples = 20, rng))
+rng = Random.seed!(2131)
+fit_infer_bs_rng_2 = infer(fit_results_allometric, Pumas.Bootstrap(;samples = 20, rng))
+coeftable(fit_infer_bs_rng_1)
+coeftable(fit_infer_bs_rng_2)
+
+## Confidence Intervals using SIR
+fit_infer_sir = infer(fit_results_allometric, Pumas.SIR(samples = 100, resamples = 10))
+coeftable(fit_infer_sir)
+
+# VPCs
+## default covariate in the X-axis is time
+fit_vpc = vpc(fit_results_allometric) # Single-Threaded
+fit_vpc = vpc(
+    fit_results_allometric;
+    bandwidth = 30,
+    prediction_correction = false, # pc-vpc
+)
+
+vpc_fig = vpc_plot(fit_vpc)
+figurelegend(vpc_fig)
+vpc_fig
+
+## stratification
+fit_vpc_allometric = vpc(
+    fit_results_allometric;
+    stratify_by = [:SEX],
+)
+vpc_plot(fit_vpc_allometric)
+
+## changing the independent variable
+fit_vpc_allometric = vpc(
+    fit_results_allometric;
+    covariates = [:CRCL],
+)
+vpc_plot(fit_vpc_allometric)
