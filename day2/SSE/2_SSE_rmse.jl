2dad7177edc7cf292e6978f32579734d3e6a0237
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2022-12-05 20:39:36 +0100

    Simple SSE example.

---
+++ day2/SSE/2_SSE_rmse.jl
@@ -0,0 +1,30 @@
+
+##################################################################
+### Instructions:
+###     1) Change to this directory
+###     2) Upload the results from the previous run to the folder
+###     3) Run the code below in the interactive session
+##################################################################
+
+using Serialization
+
+##################################################################
+### Find the jls after uploading it
+##################################################################
+result_filename = first(filter(f->contains(f, ".jls"), readdir()))
+
+##################################################################
+### Deserialize
+##################################################################
+results = deserialize(result_filename)
+
+##################################################################
+### Grab all the vectors of fits
+##################################################################
+fits = map(r->r.fits, results)
+
+##################################################################
+### Calculate RMSE for θ_CL_parent
+##################################################################
+rmse = map(r -> sqrt(mean(map(r_j -> (coef(r_j).θ_CL_parent - param.θ_CL_parent)^2, r))), fits)
+
